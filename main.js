0de340fa835567d37360e7f220bff0fea35c9c91
Alexandria <alexandriamlewis@me.com>
2018-12-15 01:44:48 -0500

    added content to about - made mobile menu

--- main.js
+++ main.js
@@ -5,4 +5,27 @@ $(function(){
     var selectTab = $(this).attr("href");
     $(selectTab).show();
   });
+  
+  $("#burger").click(function(e){
+    e.preventDefault();
+    if( $(".fa-bars").is(":visible") ){
+      $(".fa-bars").hide();
+      $(".fa-times").show();
+      $("#mobileMenu").show();
+    }else if( $(".fa-times").is(":visible") ){
+      $(".fa-times").hide();
+      $(".fa-bars").show();
+      $("#mobileMenu").hide();
+    }
+  });
+  
+  $(".linkMobile").click(function(e) {
+    e.preventDefault();
+    $('.tab_content').hide();
+    var selectTab = $(this).attr("href");
+    $(selectTab).show();
+    $(".fa-times").hide();
+    $(".fa-bars").show();
+    $("#mobileMenu").hide();
+  });
 });
